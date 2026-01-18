c237b2d02dfd5721052b66372795451451e4315c
shakoure <shakoure@socialeap.net>
2026-01-18 15:17:47 -0500

    Enable PWA service worker via Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -10,7 +11,17 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react()],
+      plugins: [
+        react(),
+        VitePWA({
+          registerType: 'autoUpdate',
+          manifest: false,
+          includeAssets: [
+            'favicon.ico',
+            'apple-touch-icon.png'
+          ],
+        }),
+      ],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
